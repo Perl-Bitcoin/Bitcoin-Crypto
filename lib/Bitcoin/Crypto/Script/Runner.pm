03c629e3ccd5617bbbc6074481c0f6362c654819
bbrtj <bbrtj.pro@gmail.com>
2023-01-18 21:53:33 +0100

    Minor fixes to Script::Runner and a documentation starter

--- lib/Bitcoin/Crypto/Script/Runner.pm
+++ lib/Bitcoin/Crypto/Script/Runner.pm
@@ -19,7 +19,7 @@ use namespace::clean;
 
 has field 'stack' => (
 	isa => ArrayRef[Str],
-	writer => 1,
+	writer => -hidden,
 );
 
 has field 'alt_stack' => (
@@ -117,9 +117,9 @@ sub _advance
 
 sub execute
 {
-	my ($self, $script) = @_;
+	my ($self, $script, $initial_stack) = @_;
 
-	$self->start($script);
+	$self->start($script, $initial_stack);
 	1 while $self->step;
 
 	return $self;
@@ -127,13 +127,13 @@ sub execute
 
 sub start
 {
-	my ($self, $script) = @_;
+	my ($self, $script, $initial_stack) = @_;
 
 	Bitcoin::Crypto::Exception::ScriptExecute->raise(
 		'invalid argument passed to script runner, not a script instance'
 	) unless blessed $script && $script->isa('Bitcoin::Crypto::Script');
 
-	$self->set_stack([]);
+	$self->_set_stack($initial_stack // []);
 	$self->_set_alt_stack([]);
 	$self->_set_pos(0);
 	$self->_set_operations($script->operations);
@@ -164,4 +164,91 @@ sub step
 }
 
 1;
+
+__END__
+=head1 NAME
+
+Bitcoin::Crypto::Script::Runner - Bitcoin script runner
+
+=head1 SYNOPSIS
+
+	use Bitcoin::Crypto::Script::Runner;
+	use Data::Dumper;
+
+	my $runner = Bitcoin::Crypto::Script::Runner->new;
+
+	# provide an instance of Bitcoin::Crypto::Script
+	# runs the script all at once
+	$runner->execute($script);
+
+	# ... or: runs the script step by step
+	$runner->start($script);
+	while ($runner->step) {
+		print 'runner step, stack: ';
+		print Dumper($runner->stack);
+	}
+
+	print 'resulting stack: ';
+	print Dumper($runner->stack);
+
+=head1 DESCRIPTION
+
+This class instances can be used to execute Bitcoin scripts defined as
+instances of L<Bitcoin::Crypto::Script>. Scripts can be executed in one go or
+step by step, and the execution stack is available through an accessor.
+
+One runner can be used to execute scripts multiple times. Each time you call
+C<execute> or C<start>, the runner state is reset. Initial stack state can be
+provided to either one of those methods. This provides better control over
+execution than L<Bitcoin::Crypto::Script/run>, which simply executes the script
+and returns its stack.
+
+=head1 INTERFACE
+
+=head2 Fields
+
+=head3 stack
+
+=head3 alt_stack
+
+=head3 pos
+
+=head3 operations
+
+=head2 Methods
+
+=head3 execute
+
+=head3 start
+
+=head3 step
+
+=head1 CAVEATS
+
+Not all opcodes are implemented. An example of unimplemented opcode is
+C<OP_CHECKLOCKTIMEVERIFY>, which would require access to transaction data.
+
+OP_0 and OP_FALSE push byte vector C<0x00> to the stack, not null-size byte
+vector.
+
+There is curretly no limit on the size of byte vector which is going to be
+transformed to an integer for ops like OP_ADD. BigInts are used for all integers.
+
+=head1 EXCEPTIONS
+
+This module throws an instance of L<Bitcoin::Crypto::Exception> if it
+encounters an error. It can produce the following error types from the
+L<Bitcoin::Crypto::Exception> namespace:
+
+=over
+
+=item * ScriptRuntime - script has encountered a runtime exception - the transaction is invalid
+
+=item * ScriptSyntax - script syntax is invalid
+
+=back
+
+=head1 SEE ALSO
+
+L<Bitcoin::Crypto::Script>
 
