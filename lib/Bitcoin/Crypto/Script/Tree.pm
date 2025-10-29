19af14a02c2ae22f6bf34452f442bc8217af1660
bbrtj <bbrtj.pro@gmail.com>
2025-10-29 17:57:50 +0100

    Add documentation for Script::Tree

--- lib/Bitcoin/Crypto/Script/Tree.pm
+++ lib/Bitcoin/Crypto/Script/Tree.pm
@@ -251,33 +251,47 @@ sub get_control_block
 
 __END__
 
-=head2 taproot_merkle_root
+=head1 NAME
 
-	$hash = taproot_merkle_root($tree_data)
+Bitcoin::Crypto::Script::Tree - BIP341 Script trees
 
-Calculates a merkle root of taproot script tree (array ref). Unlike
-L</merkle_root>, this must be an actual binary tree structure:
+=head1 SYNOPSIS
 
-	my $leaf1 = {
-		hash => [hex => $block_hash1]
-	};
+=head1 DESCRIPTION
 
-	my $leaf2 = {
-		hash => [hex => $block_hash2]
-	};
+This module contains implementation script trees described in
+L<BIP341|https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki>. These
+trees are used by taproot and are necessary to build custom taproot scripts.
 
-	my $leaf3 = {
-		leaf_version => 192,
-		script => [hex => '20c440b462ad48c7a77f94cd4532d8f2119dcebbd7c9764557e62726419b08ad4cac'],
-	};
+=head2 Tree leaves
 
-	[
-		$leaf1,
-		[
-			$leaf2,
-			$leaf3,
-		]
-	]
+Each leaf in the tree is represented with this Perl structure:
+
+	{
+		id => integer (optional),
+		leaf_version => integer,
+		script => Bitcoin::Crypto::Script instance (or its coercible),
+	}
+
+Optional C<id> is used to identify the leaf in the tree, which is used in
+methods like L</get_control_block>.
+
+If the leaf is prehashed or not known, it can be represented as this structure
+instead:
+
+	{
+		hash => bytestring with prehashed leaf,
+	}
+
+=head1 METHODS
+
+=head2 from_structure
+
+	$tree = $class->from_structure($structure)
+
+This static method builds a new C<$tree> object from structure C<$structure>.
+This structure represents a binary tree and must contain an array reference of
+array or hash references.
 
 Each level of a tree must be an array reference with up to two values in it.
 Each leaf must be a hash with either a prehashed value under C<hash> key
@@ -286,5 +300,110 @@ be hashed represented by keys C<leaf_version> (integer up to 255) and C<script>
 (an instance of L<Bitcoin::Crypto::Script> or something which can be coerced
 into it).
 
-Returns a bytestring which is the root hash of the tree.
+Example structure:
+
+	# tree with all scripts known
+	[
+		{
+			id => 0,
+			leaf_version => 192,
+			script => [hex => '2071981521ad9fc9036687364118fb6ccd2035b96a423c59c5430e98310a11abe2ac']
+		},
+		[
+			{
+				id => 1,
+				leaf_version => 192,
+				script => [hex => '20d5094d2dbe9b76e2c245a2b89b6006888952e2faa6a149ae318d69e520617748ac']
+			},
+			{
+				id => 2,
+				leaf_version => 192,
+				script => [hex => '20c440b462ad48c7a77f94cd4532d8f2119dcebbd7c9764557e62726419b08ad4cac']
+			}
+		]
+	]
+
+Each element may be prehashed, which represents the same tree, but without
+disclosing information about a script:
+
+	# same tree, but only one script is known and can be executed
+	[
+		{hash => [hex => 'f154e8e8e17c31d3462d7132589ed29353c6fafdb884c5a6e04ea938834f0d9d']},
+		[
+			{
+				leaf_version => 192,
+				script => [hex => '20d5094d2dbe9b76e2c245a2b89b6006888952e2faa6a149ae318d69e520617748ac']
+			},
+			{hash => [hex => 'd7485025fceb78b9ed667db36ed8b8dc7b1f0b307ac167fa516fe4352b9f4ef7']},
+		]
+	]
+
+=head2 from_path
+
+	$tree = $class->from_path($leaf, \@path)
+
+This static method builds a new C<$tree> object from key path C<@path>. Key
+path and represents a script tree by a series of prehashed leaves. C<$leaf> is
+a tree leaf deserialized from transaction data.
+
+C<@path> must only contain bytestrings or their coercibles. Example C<@path>
+could look like this:
+
+	(
+		[hex => 'd7485025fceb78b9ed667db36ed8b8dc7b1f0b307ac167fa516fe4352b9f4ef7'],
+		[hex => 'f154e8e8e17c31d3462d7132589ed29353c6fafdb884c5a6e04ea938834f0d9d'],
+	)
+
+=head2 get_merkle_root
+
+	$hash = $tree->get_merkle_root()
+
+Calculates a merkle root of the script tree. Returns a bytestring which is the
+root hash of the tree.
+
+=head2 get_tapleaf_hash
+
+	$hash = $tree->get_tapleaf_hash($leaf_id)
+
+Calculates a tapleaf hash of a leaf with given C<$leaf_id>. If such leaf does
+not exist, an exception is thrown. Returns a bytestring.
+
+=head2 get_control_block
+
+	$bytestr = $tree->get_control_block($leaf_id, $pubkey)
+
+Builds a binary taproot control block used in taproot witness data. C<$leaf_id>
+must be a valid identifier of a leaf existing in the tree. C<$pubkey> is a
+public key that associated with the address for key path spending.
+
+=head2 get_tree_paths
+
+	$paths = $tree->get_tree_paths()
+
+Returns a hash reference of paths for each of leaves in the tree which have an
+id. Each path is an array reference - same as what L</from_path> takes as input.
+
+=head1 EXCEPTIONS
+
+This module throws an instance of L<Bitcoin::Crypto::Exception> if it
+encounters an error. It can produce the following error types from the
+L<Bitcoin::Crypto::Exception> namespace:
+
+=over
+
+=item * ScriptTree - general error with the tree
+
+=back
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Bitcoin::Crypto::Tapscript>
+
+=item L<Bitcoin::Crypto::Transaction>
+
+=back
+
+=cut
 
